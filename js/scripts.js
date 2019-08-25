967f8251ec98111f1c6de14c66a4220ac6e6f94e
Nelson Kimani <nelsonkimani96@gmail.com>
2019-08-25 08:03:06 +0300

    add an hover effect and set background color

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,9 @@ $(document).ready(function (){
      $(this).children('.hideMe').slideoggle(1200);
       $(this).children('.show').toggle(1500);
     });
+    $('.card').hover(function(){
+    $(this).css("background-color", "transparent");
+   }, function(){
+   $(this).css("background-color", "transparent");
+  });
 });
