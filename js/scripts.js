b6d316f73dbb98b7e94eab5c7807ea0cba3c99f9
Nelson Kimani <nelsonkimani96@gmail.com>
2019-08-26 11:21:52 +0300

    Fix jquery bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
-$(document).ready(function(){
-   $('.main').click(function () {
-     $(this).children('.show').toggle(1200);
-      $(this).children('.hideMe').slideToggle(1500);
-    });
-    $('.card').hover(function(){
+$(document).ready(function() {
+  $('.main').click(function() {
+    $(this).children('.show').toggle(1200);
+    $(this).children('.hideMe').slideToggle(1500);
+  });
+  $('.card').hover(function() {
     $(this).css("background-color", "transparent");
-   }, function(){
-   $(this).css("background-color", "transparent");
+  }, function() {
+    $(this).css("background-color", "transparent");
+  });
+  // form submission effect
+  $("form").on('submit', function(e) {
+    alert("we have received your message. Thank you for reaching out to us.");
+    return false;
   });
 });
